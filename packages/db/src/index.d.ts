28149a429c81b06d413e6181c82f2ad3fb944ef2
00016424wiut <00016424wiut@gmail.com>
2026-04-10 18:35:36 +0500

    fixing database

--- packages/db/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "../prisma/generated/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-export declare function createPrismaClient(): PrismaClient<{
-    adapter: PrismaPg;
-}, never, import("../prisma/generated/runtime/client").DefaultArgs>;
-declare const prisma: PrismaClient<{
-    adapter: PrismaPg;
-}, never, import("../prisma/generated/runtime/client").DefaultArgs>;
-export default prisma;
